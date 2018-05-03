aa02f74d00d84dd6320cd4b0db8ee97a64d2926f
Neil Johnson <realdealneil@gmail.com>
2018-05-03 13:23:27 -0600

    Added a couple more tests just to be sure

--- src/gpsConverterTest.cpp
+++ src/gpsConverterTest.cpp
@@ -32,6 +32,32 @@ int main(void)
 	std::cout << "  West Point  Distances from Home: " << d << " (Should be 0 N, -1000 E)" << std::endl;
 	
 	std::cout << std::endl;
+	
+	//! Now the off-axis directions:
+	simplegps::latLon NELoc = {.lat = 40.584413209, .lon = -111.96232224};
+	
+	simplegps::latLon SELoc = {.lat = 40.566416791, .lon = -111.96232224};
+	
+	simplegps::latLon SWLoc = {.lat = 40.566416791, .lon = -111.98601576}; 	
+	
+	simplegps::latLon NWLoc = {.lat = 40.584413209, .lon = -111.98601576};
+	
+	//! First, Test that we get the right distances for 1000 m for each of the points above.  
+	std::cout << "LatLon Distance calculation test: " << std::endl;
+	
+	simplegps::meterDistFromLatLon(homeLoc, NELoc, d);	
+	std::cout << "  NorthEast Point Distances from Home: " << d << " (Should be 1000 N, 1000 E)" << std::endl;
+	
+	simplegps::meterDistFromLatLon(homeLoc, SELoc, d);
+	std::cout << "  SouthEast Point  Distances from Home: " << d << " (Should be -1000 N, 1000 E)" << std::endl;
+	
+	simplegps::meterDistFromLatLon(homeLoc, SWLoc, d);
+	std::cout << "  SouthWest Point Distances from Home: " << d << " (Should be -1000 N, -1000 E)" << std::endl;
+	
+	simplegps::meterDistFromLatLon(homeLoc, NWLoc, d);
+	std::cout << "  NorthWest Point  Distances from Home: " << d << " (Should be 1000 N, -1000 E)" << std::endl;
+	
+	std::cout << std::endl;
 		
 	//! Next, test the inverse (getting new lat lon from given distance):
 	d = {.pNorth = 1000, .pEast = 0};
@@ -52,6 +78,22 @@ int main(void)
 	simplegps::LatLonFromHomeDistance(d, homeLoc, newLoc);
 	std::cout << "  West 1000 m: " << newLoc << " (should be about 40.575415, -111.98601576)\n";
 	
+	std::cout << "\nOff Axis:\n";
+	d = {.pNorth = 1000, .pEast = 1000};
+	simplegps::LatLonFromHomeDistance(d, homeLoc, newLoc);
+	std::cout << "  NorthEast 1000 m: " << newLoc << " (should be about 40.584413209, -111.96232224)\n";
+	
+	d = {.pNorth = -1000, .pEast = 1000};
+	simplegps::LatLonFromHomeDistance(d, homeLoc, newLoc);
+	std::cout << "  SouthEast 1000 m: " << newLoc << " (should be about 40.566416791, -111.96232224)\n";
+
+	d = {.pNorth = -1000, .pEast = -1000};
+	simplegps::LatLonFromHomeDistance(d, homeLoc, newLoc);
+	std::cout << "  SouthWest 1000 m: " << newLoc << " (should be about 40.566416791, -111.98601576)\n";
+
+	d = {.pNorth = 1000, .pEast = -1000};
+	simplegps::LatLonFromHomeDistance(d, homeLoc, newLoc);
+	std::cout << "  NorthWest 1000 m: " << newLoc << " (should be about 40.584413209, -111.98601576)\n";
 	
 	
 	
